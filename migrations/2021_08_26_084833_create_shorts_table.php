4f6ffb3fa8dd76953471d5864d602e18b2de4e0b
home <i44711015@gmail.com>
2021-08-26 08:53:29 +0800

    更新

---
+++ migrations/2021_08_26_084833_create_shorts_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Hyperf\Database\Schema\Schema;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Migrations\Migration;
+
+class CreateShortsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shorts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shorts');
+    }
+}
